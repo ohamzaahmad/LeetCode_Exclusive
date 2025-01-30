3c02fd072d29bf8e05fd8db00f0ac089de9934e9
ohamzaahmad <117470177+ohamzaahmad@users.noreply.github.com>
2025-01-31 00:17:13 +0500

    Time: 0 ms (100%), Space: 18.3 MB (55.21%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,17 +1,19 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        
-        result = []
-        
-        for i in range(1, n + 1):
-            if i % 3 == 0 and i % 5 == 0:
-                result.append("FizzBuzz")
-            elif i % 3 == 0:
+        result =[]
+
+        for array_index in range(n):
+            fake_i = array_index + 1
+
+            #if divisible by 3 and NOT 5
+            if fake_i % 3 == 0 and fake_i % 5 != 0:
                 result.append("Fizz")
-            elif i % 5 == 0:
+            # if divisible by 5 and NOT 3
+            elif fake_i % 5 == 0 and fake_i % 3 != 0:
                 result.append("Buzz")
+            # if divisible by both 3 and 5
+            elif fake_i % 3 == 0 and fake_i % 5 == 0:
+                result.append("FizzBuzz")
             else:
-                result.append(str(i))
-        
+                result.append(str(fake_i))
         return result
-#solved by chatgpt bcz streak
