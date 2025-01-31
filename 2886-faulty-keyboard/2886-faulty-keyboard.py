2f3b64550afab411a8d437122d1d7e9774096bd3
ohamzaahmad <117470177+ohamzaahmad@users.noreply.github.com>
2025-01-31 19:45:13 +0500

    Time: 0 ms (100%), Space: 17.9 MB (12.03%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.py
+++ 2886-faulty-keyboard/2886-faulty-keyboard.py
@@ -1,22 +1,11 @@
 class Solution:
     def finalString(self, s: str) -> str:
-        result =[]
-        # 2 pointer approach
-        for char in s: # check character by char in string
-            if char  == 'i':
-                # first pointer on index 1
-                left = 0
-                # Second on last
-                right = len(result)-1
-                # initiated infinite until condition met
-                while left < right:
-                    # tuple unpacking technique by swapping value
-                    result[left], result[right] = result[right], result[left]
-                    # changing pointer by increasing 1 for left and decreasing 1 for left side
-                    left += 1
-                    right -=1
+        result = []  # A list to hold the characters
+        
+        for char in s:
+            if char == 'i':
+                result.reverse()  # Reverse the list when encountering 'i'
             else:
-                # else simply add that character which is abviously other than "i" to array
-                result.append(char)
-        # if the loops condition breaks after meeting the condition, it will join all character(stored in result array: collection of array) to make single word.
-        return "".join(result)
+                result.append(char)  # Append other characters normally
+                
+        return "".join(result)  # Convert the list to a string at the end
