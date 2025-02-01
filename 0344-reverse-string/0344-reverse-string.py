58b2fc9da0d3f5c9d456cf384bc7c293460490ed
ohamzaahmad <117470177+ohamzaahmad@users.noreply.github.com>
2025-02-01 21:01:33 +0500

    Time: 0 ms (100%), Space: 23 MB (35.17%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -13,15 +13,15 @@ class Solution:
 
 
 
+        s.reverse()
 
 
 
-
-        # left =0
-        # right= len(s)-1
-        left, right = 0, len(s)-1 # tuple unpacking method
-        while left<right:
-            s[left], s[right] = s[right], s[left]
-            left+=1
-            right-=1
-           # tuple is like list: tuple is unchangeable: support indexing, faster than list
+        # # left =0
+        # # right= len(s)-1
+        # left, right = 0, len(s)-1 # tuple unpacking method
+        # while left<right:
+        #     s[left], s[right] = s[right], s[left]
+        #     left+=1
+        #     right-=1
+        #    # tuple is like list: tuple is unchangeable: support indexing, faster than list
