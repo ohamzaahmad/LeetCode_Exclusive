25b2344b9b235622474c5aa7f11ec33def5850cb
ohamzaahmad <117470177+ohamzaahmad@users.noreply.github.com>
2025-02-01 20:52:09 +0500

    Time: 3 ms (43.61%), Space: 22.9 MB (67.75%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,6 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        
-        s[:] =s[::-1]
-        # return s[::-1]
+        # left =0
+        # right= len(s)-1
+        left, right = 0, len(s)-1 # tuple unpacking method
+        while left<right:
+            s[left], s[right] = s[right], s[left]
+            left+=1
+            right-=1
+           # tuple is like list: tuple is unchangeable: support indexing, faster than list
