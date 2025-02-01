db4330a6c84443763c178d6646c37276d0a419d5
ohamzaahmad <117470177+ohamzaahmad@users.noreply.github.com>
2025-02-01 21:01:03 +0500

    Time: 2 ms (49.61%), Space: 23 MB (35.17%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,24 +4,24 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
 
-        def reverse_str(left, right):
-            if left < right:
-                s[left], s[right] = s[right], s[left]
-                reverse_str(left+1, right-1)
+        # def reverse_str(left, right):
+        #     if left < right:
+        #         s[left], s[right] = s[right], s[left]
+        #         reverse_str(left+1, right-1)
 
-        reverse_str(0, len(s)-1)
-
-
+        # reverse_str(0, len(s)-1)
 
 
 
 
 
-        # # left =0
-        # # right= len(s)-1
-        # left, right = 0, len(s)-1 # tuple unpacking method
-        # while left<right:
-        #     s[left], s[right] = s[right], s[left]
-        #     left+=1
-        #     right-=1
-        #    # tuple is like list: tuple is unchangeable: support indexing, faster than list
+
+
+        # left =0
+        # right= len(s)-1
+        left, right = 0, len(s)-1 # tuple unpacking method
+        while left<right:
+            s[left], s[right] = s[right], s[left]
+            left+=1
+            right-=1
+           # tuple is like list: tuple is unchangeable: support indexing, faster than list
