ba9d93f23416dbeb50b11a8da197803d00239126
ohamzaahmad <117470177+ohamzaahmad@users.noreply.github.com>
2025-02-02 20:19:58 +0500

    Time: 0 ms (100%), Space: 17.7 MB (47.79%) - LeetHub

---
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,13 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        
+        count_breaks = 0
+        n = len(nums)
+        
+        for i in range(n):
+            if nums[i] > nums[(i + 1) % n]:  # Compare with the next element (wrap-around)
+                count_breaks += 1
+                if count_breaks > 1:
+                    return False
+        
+        return True
