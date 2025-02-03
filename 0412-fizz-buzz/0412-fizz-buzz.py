c8ab289134df82114fe78d9f4bf832e6c62f6827
ohamzaahmad <117470177+ohamzaahmad@users.noreply.github.com>
2025-02-03 20:32:17 +0500

    Time: 4 ms (6.17%), Space: 18.5 MB (22.39%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,19 +1,15 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         result =[]
-
-        for array_index in range(n):
-            fake_i = array_index + 1
-
-            #if divisible by 3 and NOT 5
-            if fake_i % 3 == 0 and fake_i % 5 != 0:
-                result.append("Fizz")
-            # if divisible by 5 and NOT 3
-            elif fake_i % 5 == 0 and fake_i % 3 != 0:
-                result.append("Buzz")
-            # if divisible by both 3 and 5
-            elif fake_i % 3 == 0 and fake_i % 5 == 0:
+        for zero_index in range(n):
+            array_index = zero_index+1
+            if not array_index % 3 and not array_index % 5:
                 result.append("FizzBuzz")
+            elif not array_index % 3:
+                result.append("Fizz")
+            elif not array_index % 5:
+                result.append("Buzz")
             else:
-                result.append(str(fake_i))
+                result.append(str(array_index))
+        
         return result
