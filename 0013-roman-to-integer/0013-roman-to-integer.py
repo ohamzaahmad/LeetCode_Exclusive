28a9799158b1b388b4fb7e438d966d256df24a03
Hamza Ahmad <117470177+ohamzaahmad@users.noreply.github.com>
2025-02-05 21:36:56 +0500

    Time: 11 ms (16.13%), Space: 17.6 MB (84.18%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -21,9 +21,13 @@ class Solution:
             # // validation for if length of array is bigger and if value is larger than next one        
             if (i+1 < len(s)) and (roman[s[i]] < roman[s[i+1]]):
                 # IM
-                # 999
+                # 999 
+                # -100 + 1000 => 900 => CM => CMXCIX
                 # 500 + 100+ 100+100+ 100+ 50+10+10+10+10+9
                 # D     C    C    C    C   L  X   X  X  X IX
+                #999 1000
+                #99 -> -10+100 -> 90 -> XCIX
+                #9 -> -1+10
                 res -= roman[s[i]]
             else:
                 res += roman[s[i]]
